14c1654a48e48bbbd4b7f931794e92c3a2077a21
VladislavPaddeus <paddeusvladislav@gmail.com>
2023-08-31 19:11:17 +0400

    Initial commit

---
+++ src/main/java/com/example/telegraminvestmentBot/TelegramInvestmentBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegraminvestmentBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramInvestmentBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramInvestmentBotApplication.class, args);
+	}
+
+}
